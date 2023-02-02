1e7029c34fe4c1f75113a51cfb99b20bb09512c0
Hassnain Ali <hassnainali1109@gmail.com>
2023-02-02 15:14:29 +0000

    Added user_type and name to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  enum user_type: { manager: 0, developer: 1, QA: 2 }
+  validates :user_type, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
