dbb45c3c6960f3c576215d87df1dd9c220d3a0f2
Hassnain Ali <hassnainali1109@gmail.com>
2023-02-08 14:48:37 +0000

    bugs functionality updated 3.0

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -3,9 +3,10 @@ class Bug < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
   belongs_to :solver, class_name: 'User', foreign_key: :solver_id
 
-  mount_uploader :screenshot, ScreenshotUploader
-
   validates :title, presence: true, uniqueness: { scope: :project_id }
+  validates :description, presence: true
+  validates :deadline, presence: true
+  mount_uploader :screenshot, ScreenshotUploader
   validates :type, presence: true
   validates :status, presence: true
 
