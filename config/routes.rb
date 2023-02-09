772d1f0112b9bfe5ca0222e55c69754156455f56
Hassnain Ali <hassnainali1109@gmail.com>
2023-02-09 14:55:09 +0000

    edited some views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+  get '/about', to: 'home#about'
+  get '/help', to: 'home#help'
   devise_for :users
    resources :projects do
     resources :user_projects
